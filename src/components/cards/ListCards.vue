7b7be1ccd1041c919a39e75c72f4de33ce36f309
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-13 17:53:52 +0200

    add: cards counter

--- src/components/cards/ListCards.vue
+++ src/components/cards/ListCards.vue
@@ -17,6 +17,7 @@ export default {
 
 <template>
   <section class="container my-2">
+    <div class="number-cards">Found {{ store.yugiCards.length }} cards</div>
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
       <Cards
         class="col"
@@ -28,4 +29,12 @@ export default {
   </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.number-cards {
+  background-color: black;
+  color: white;
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 0.5rem;
+}
+</style>
